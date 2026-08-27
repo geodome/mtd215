2c1e051e58baee0952b72bbc8d469a7764a59e61
Donaldson Tan <geodome@gmail.com>
2026-08-27 08:52:14 +0800

    completed question 3

--- TMAs/2025/question3.cpp
+++ TMAs/2025/question3.cpp
@@ -37,9 +37,41 @@ void display_numbers(int* numbers) {
 
 }
 
+bool sorted(int* numbers) {
+    for(int i = 1; i<10;i++) {
+        if(numbers[i] < numbers[i-1]) return false;
+    }
+    return true;
+}
+
+bool is_adjacent(int i, int j) {
+    return abs(i - j) == 1;
+}
+
 int main() {
     int numbers[10];
+    int index1, index2;
+
     generate_numbers(numbers);
-    display_numbers(numbers);
+    
+    while(!sorted(numbers)) {
+        display_numbers(numbers);
+
+        // prompt user for index1 and index2
+        std::cout << "Enter first index: ";
+        std::cin >> index1;
+        std::cout << "Enter second index: ";
+        std::cin >> index2;
+
+        // check for adjacency
+        if(is_adjacent(index1, index2)) {
+            // if indices are adjacent, swap the elements at the indices
+            int temp = numbers[index1];
+            numbers[index1] = numbers[index2];
+            numbers[index2] = temp;
+        } else 
+            // error
+            std::cout << "Error: Cannot swap non-adjacent elements" << std::endl;
+    }
     return 0;
 }
