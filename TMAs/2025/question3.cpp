f833133695b82f23cf41191044f5b66bfdc7ca21
Donaldson Tan <geodome@gmail.com>
2026-08-29 15:29:14 +0800

    updated to declaration-definition format'

--- TMAs/2025/question3.cpp
+++ TMAs/2025/question3.cpp
@@ -1,16 +1,41 @@
 #include <iostream>
 #include <string>
 
-bool is_unique(int n, int* numbers, int i) {
-    for(int j=0; j < i; j++) {
-        if(numbers[j] == n) return false;
+void generate_numbers(int numbers[], int size);
+void display_numbers(int numbers[], int size);
+bool sorted(int numbers[], int size);
+bool is_adjacent(int index1, int index2);
+bool is_unique(int n, int numbers[], int i);
+
+// ask name and score for 12 players
+int main(int argc, char* argv[]) {
+    int num[10];
+    int index1, index2;
+    generate_numbers(num, 10);
+    while(!sorted(num, 10)) {
+        display_numbers(num, 10);
+        std::cout << "Enter index 1: ";
+        std::cin >> index1;
+        std::cout << "Enter index 2: ";
+        std::cin >> index2;
+        if(is_adjacent(index1, index2)) {
+            // swap the adjacent elements
+            int temp = num[index1];
+            num[index1] = num[index2];
+            num[index2] = temp;
+            std::cout << "The elements are swapped" << std::endl;
+        } else
+            // error. don't swap non-adjacent elements.
+            std::cout << "Error: Non-adjacent indices" << std::endl;
     }
-    return true;
+    // congrat user for sorting the number
+    display_numbers(num,10);
+    std::cout << "congrats" << std::endl;
 }
 
-void generate_numbers(int* numbers) {
+void generate_numbers(int numbers[], int size) {
     int i = 0;
-    while(i<10) {
+    while(i < size) {
         int n = rand() % 10000;
         if(is_unique(n, numbers, i)) {
             numbers[i] = n;
@@ -19,65 +44,28 @@ void generate_numbers(int* numbers) {
     }
 }
 
-std::string zeroes(int n) {
-    if(n < 10) return "000";
-    if(n < 100) return "00";
-    if(n < 1000) return "0";
-    return "";
+bool is_unique(int n, int numbers[], int i) {
+    for(int j = 0; j < i; j++) {
+        if(numbers[j] == n) return false;
+    }
+    return true;
 }
 
-void display_numbers(int* numbers) {
-    for(int i=0; i<10;i++) {
-        std::cout << zeroes(numbers[i]) << numbers[i] << " ";        
-    }
-    std::cout << std::endl;
-    for(int i=0; i<10;i++) {
-        std::cout << "====" << " ";        
+void display_numbers(int numbers[], int size) {
+    for(int i=0; i < size; i++) {
+        std::cout << numbers[i] << "  ";
     }
     std::cout << std::endl;
 }
 
-bool sorted(int* numbers) {
-    for(int i = 1; i<10;i++) {
+bool sorted(int numbers[], int size) {
+    for(int i=1; i<size; i++) {
         if(numbers[i] < numbers[i-1]) return false;
     }
     return true;
 }
 
-bool is_adjacent(int i, int j) {
-    return abs(i - j) == 1;
-}
-
-int main() {
-    int numbers[10];
-    int index1, index2;
-
-    generate_numbers(numbers);
-    
-    while(!sorted(numbers)) {
-        display_numbers(numbers);
-
-        // prompt user for index1 and index2
-        std::cout << "Enter first index: ";
-        std::cin >> index1;
-        std::cout << "Enter second index: ";
-        std::cin >> index2;
-
-        // check for adjacency
-        if(is_adjacent(index1, index2)) {
-            // if indices are adjacent, swap the elements at the indices
-            int temp = numbers[index1];
-            numbers[index1] = numbers[index2];
-            numbers[index2] = temp;
-            std::cout << "indices " << index1 << " and " << index2 << " are swapped." << std::endl;
-        } else 
-            // error
-            std::cout << "Error: Cannot swap non-adjacent elements" << std::endl;
-    }
-
-    // congrats message
-    display_numbers(numbers);
-    std::cout << "Congratulations, the numbers are now sorted in increasing order." << std::endl;
-
-    return 0;
+bool is_adjacent(int index1, int index2) {
+    int d = index1 - index2;
+    return d == 1 || d == -1;
 }
