500a094c840f9459336c442eff3d2cc884d0b341
Donaldson Tan <geodome@gmail.com>
2026-08-31 06:36:33 +0800

    added comments

--- TMAs/2025/question3.cpp
+++ TMAs/2025/question3.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 
+// Due to array decay, when passing an array as a function parameter, always include the array size so as to ensure memory safety.
 void generate_numbers(int numbers[], int size);
 void display_numbers(int numbers[], int size);
 std::string zeroes(int n);
@@ -35,6 +36,9 @@ int main(int argc, char* argv[]) {
     return 0;
 }
 
+/*
+This generates an array of 10 numbers, each from 0 to 9999.
+*/
 void generate_numbers(int numbers[], int size) {
     int i = 0;
     while(i < size) {
@@ -46,6 +50,9 @@ void generate_numbers(int numbers[], int size) {
     }
 }
 
+/*
+This checks that the subarray numbers[0..i-1] doesn't contain n
+*/
 bool is_unique(int n, int numbers[], int i) {
     for(int j = 0; j < i; j++) {
         if(numbers[j] == n) return false;
@@ -53,6 +60,12 @@ bool is_unique(int n, int numbers[], int i) {
     return true;
 }
 
+/*
+This produces the following output:
++------+------+------+------+------+------+------+------+------+------+
+| 6807 + 5249 + 0073 + 3658 + 8930 + 1272 + 7544 + 0878 + 7923 + 7709 |
++------+------+------+------+------+------+------+------+------+------+
+*/
 void display_numbers(int numbers[], int size) {
     std::cout << std::endl << "+";
     for(int i=0; i < size; i++) {
@@ -71,6 +84,9 @@ void display_numbers(int numbers[], int size) {
 
 }
 
+/*
+Returns string of zeros to pad n to a width of 4 characters. Assumes n is a number between 0 and 9999.
+*/
 std::string zeroes(int n) {
     if(n < 10) return "000";
     if(n < 100) return "00";
@@ -78,7 +94,9 @@ std::string zeroes(int n) {
     return "";
 }
 
-
+/*
+Checks if the array numbers[] is sorted in ascending order.
+*/
 bool is_sorted(int numbers[], int size) {
     for(int i=1; i<size; i++) {
         if(numbers[i] < numbers[i-1]) return false;
@@ -86,6 +104,9 @@ bool is_sorted(int numbers[], int size) {
     return true;
 }
 
+/*
+Checks if index1 and index2 represent adjacent elements.
+*/
 bool is_adjacent(int index1, int index2) {
     int d = index1 - index2;
     return d == 1 || d == -1;
